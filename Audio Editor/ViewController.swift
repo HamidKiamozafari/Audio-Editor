a1d1aeea5a2a7dd116839a2dbb50500b80d77d13
David Ilenwabor <dave11ilen@gmail.com>
2018-05-19 00:48:17 +0100

    Connected opening storyboard Views to View Controller

---
+++ Audio Editor/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Pitch Perfect
+//
+//  Created by David Ilenwabor on 5/18/18.
+//  Copyright © 2018 DJTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordingButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("View will appear called")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("View Did appear called")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func recordAudio(_ sender: AnyObject) {
+        
+    print("Record button was pressed")
+        recordingLabel.text = "Recording in Progress"
+    }
+    @IBAction func stopRecording(_ sender: Any) {
+        print("stop recording button was pressed")
+    }
+}
+
+
